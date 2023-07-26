1409eeb3c369b142369dbae75d1126051a79afaa
Angad <angadmisra28@gmail.com>
2023-07-25 22:40:20 -0700

    Add VR offset

--- src/Hooks.cpp
+++ src/Hooks.cpp
@@ -6,7 +6,7 @@ namespace Hooks {
     void InstallHook() {
         auto& trampoline = SKSE::GetTrampoline();
 
-        REL::Relocation<std::uintptr_t> func{REL::RelocationID(35565, 36564), REL::Relocate(0x748, 0xC26)};
+        REL::Relocation<std::uintptr_t> func{REL::RelocationID(35565, 36564), REL::Relocate(0x748, 0xc26, 0x7ee)};
 
         GammaController::_OnFrameUpdate = trampoline.write_call<5>(func.address(), GammaController::OnFrameUpdate);
 
